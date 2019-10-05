934a51b6c958cf583e3cff18dc960ac2132ee830
Kneeslapper <gloriaklee8@gmail.com>
2019-10-05 00:16:04 -0700

    added the bonus,

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     console.log("ready");
-
+    var update;
     var firebaseConfig = {
         apiKey: "AIzaSyDk7FfMFN-PloHbu_7oBE6I-wpNFjMaC8M",
         authDomain: "trains-45bf8.firebaseapp.com",
@@ -12,37 +12,51 @@ $(document).ready(function () {
     }
     //initialize firebase------------------------------------->
     firebase.initializeApp(firebaseConfig);
+    refresh();
+    var timer = setInterval(function () { refresh() }, 60000);
+    function refresh() {
+        $('table tbody').empty();
+        firebase.database().ref().once('value').then(function (childSnapshot) {
+            var items = childSnapshot.val();
+            console.log(items)
 
-    // var database = firebase.database();
-    firebase.database().ref().once('value').then(function (childSnapshot) {
-        var items = childSnapshot.val();
-        console.log(items)
+            //need to add the values into the table detail
+            for (var i in items) {
+                console.log(typeof items[i].time)
+                var freq = parseInt(items[i].frequency)
+                var min = moment(items[i].time, ('HH:mm') // convert to military time// 
+                ).subtract(1, "years");
+                console.log(min);
 
-        //need to add the values into the table detail
-        for (var i in items) {
-            var freq = parseInt(items[i].frequency)
-            var min = moment(items[i].time, "HH:mm" // convert to military time// 
-            ).subtract(1, "years");
-            console.log(min);
-            console.log(min, freq)
-            var tRemainder = min % freq;
-            console.log(tRemainder)
-            var tMinutesTillTrain = freq - tRemainder;
-            var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-            console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-            var template = '<tr>' +
-                '<td>' + items[i].trainName + '</td>' +
-                '<td>' + items[i].destination + '</td>' +
-                '<td>' + items[i].frequency + '</td>' +
-                '<td>' + moment(nextTrain).format("hh:mm") + '</td>' +
-                '<td>' + tMinutesTillTrain + '</td>' +
-                '</tr>'
-            $('table tbody').append(template)
 
-            console.log(min);
-        }
+                console.log(min, freq)
 
-    })
+                var diffTime = moment().diff(moment(min), "minutes");
+                console.log("DIFFERENCE IN TIME: " + diffTime);
+
+
+                var tRemainder = diffTime % freq;
+                console.log(tRemainder)
+                var tMinutesTillTrain = freq - tRemainder;
+                var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+                console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+                var template = '<tr>' +
+                    '<td>' + items[i].trainName + '</td>' +
+                    '<td>' + items[i].destination + '</td>' +
+                    '<td>' + items[i].frequency + '</td>' +
+                    '<td>' + moment(nextTrain).format("hh:mm") + '</td>' +
+                    '<td>' + tMinutesTillTrain + '</td>' +
+                    '</tr>'
+                $('table tbody').append(template)
+
+                console.log(min);
+            }
+
+        });
+    };
+    //bonus
+    console.log(timer)
+
 
 
 
@@ -56,17 +70,16 @@ $(document).ready(function () {
         console.log(trainData);
         //changed the value to match the value in the database
         console.log(trainName);
-        var id = Math.floor(Math.random() * 1000);
         //set new value to firebase
         firebase.database().ref().push({
             trainName: trainData[0].value,
             destination: trainData[1].value,
             frequency: trainData[3].value,
-            time: trainData[2].value
+            time: trainData[2].value,
 
         })
         $('form').trigger("reset")
-
+        // refresh();
 
     });
 
