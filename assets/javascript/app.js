d355d72b860836c8660fb7c863c8acb62569af89
Kneeslapper <gloriaklee8@gmail.com>
2019-10-04 21:47:38 -0700

    current time divide frequency, gives reminader, remainder plus current time equals next time arrival

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,81 @@
 $(document).ready(function () {
     console.log("ready");
-    var firebaseConfig = {
-        apiKey: "AIzaSyAR-jQQNcEkwpK-6aBJKryGzITYOtuwk84",
-        authDomain: "train-8e77b.firebaseapp.com",
-        databaseURL: "https://train-8e77b.firebaseio.com",
-        projectId: "train-8e77b",
-        storageBucket: "train-8e77b.appspot.com",
-        messagingSenderId: "22939645081",
-        appId: "1:22939645081:web:587363528e8b09e049b408",
-        measurementId: "G-WNHJHGYPB7"
-    };
-    firebase.initializeApp(firebaseConfig)
-    //jquery submit form
+    var d = new Date(); //without params it defaults to "now"
+    var t = d.getHours() + ":" + d.getMinutes();
 
+    var firebaseConfig = {
+        apiKey: "AIzaSyDk7FfMFN-PloHbu_7oBE6I-wpNFjMaC8M",
+        authDomain: "trains-45bf8.firebaseapp.com",
+        databaseURL: "https://trains-45bf8.firebaseio.com",
+        projectId: "trains-45bf8",
+        storageBucket: "trains-45bf8.appspot.com",
+        messagingSenderId: "389339760642",
+        appId: "1:389339760642:web:2242510474184025b33f1b"
+    }
+    //initialize firebase------------------------------------->
+    firebase.initializeApp(firebaseConfig);
+
+    var database = firebase.database();
+    firebase.database().ref().once('value').then(function (childSnapshot) {
+        var items = childSnapshot.val();
+        console.log(items)
+
+        //need to add the values into the table detail
+        for (var i in items) {
+            var freq = parseInt(items[i].frequency)
+            var min = moment(items[i].time, "HH:mm" // convert to military time// 
+            ).subtract(1, "years");
+            console.log(min);
+            console.log(min, freq)
+            var tRemainder = min % freq;
+            console.log(tRemainder)
+            var tMinutesTillTrain = freq - tRemainder;
+            var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+            console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+            var template = '<tr>' +
+                '<td>' + items[i].trainName + '</td>' +
+                '<td>' + items[i].destination + '</td>' +
+                '<td>' + items[i].frequency + '</td>' +
+                '<td>' + moment(nextTrain).format("hh:mm") + '</td>' +
+                '<td>' + tMinutesTillTrain + '</td>' +
+                '</tr>'
+            $('table tbody').append(template)
+
+            console.log(min); t
+        }
+
+    })
+
+
+
+
+    console.log(firebaseConfig);
     $("form").on("submit", function (event) {
-        console.log($(this).serializeArray())
+        //need to put an event.preventDefault to stop the page from auto refresh
         event.preventDefault();
 
         var trainData = $(this).serializeArray()
         console.log(trainData);
-
-        var trainName = trainData[0].value;
+        //changed the value to match the value in the database
         console.log(trainName);
-        var destination = traindata[1].value;
-        var frequency = traindata[3].value;
-        var time = traindata[2].value
+        var id = Math.floor(Math.random() * 1000);
+        //set new value to firebase
+        database.ref().push({
+            trainName: trainData[0].value,
+            destination: trainData[1].value,
+            frequency: trainData[3].value,
+            time: trainData[2].value
+
+        })
+        $('form').trigger("reset")
+
 
     });
 
-
-
-
-
 });
+
+
+
+
+
+
